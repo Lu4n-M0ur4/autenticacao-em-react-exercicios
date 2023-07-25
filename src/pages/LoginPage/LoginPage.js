465cf41b014f59b3a6a7acc53bb20cd172e25ef1
Lu4n-M0ur4 <luan.moura-rj@hotmail.com>
2023-07-25 17:33:13 -0300

    exercicio realizado

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -2,18 +2,40 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { goToFeed, goToSignUp } from "../../routes/coordinator.js";
 import { FormContainer, InputContainer } from "./styled.js";
-
+import useForm from "../../Hooks/useForm.js";
+import axios from "axios";
+import { baseURL } from "../../constants/baseURL.js";
 
 function LoginPage() {
   const navigate = useNavigate();
- 
+
+  const { form, onChangeInput, clearFild } = useForm({email:'',password:''});
+
+  const handleLoguin = (e) => {
+    e.preventDefault();
+    const body = {
+      email:form.email,
+      password:form.password
+    }
+
+    axios.post(`${baseURL}/user/login`,body)
+    .then((res)=>{
+      console.log(res)
+      localStorage.setItem("token", res.data.token)
+    }).catch((res) => {console.log(res)})
+
+  }
+
   return (
     <main>
       <h1>Login</h1>
-      <FormContainer>
+      <FormContainer onSubmit={handleLoguin}>
         <InputContainer>
           <label htmlFor="email">E-mail:</label>
           <input
+            name="email"
+            value={form.email}
+            onChange={onChangeInput}
             id="email"
             required
           />
@@ -22,10 +44,13 @@ function LoginPage() {
           <label htmlFor="password">Senha:</label>
           <input
             id="password"
+            name="password"
+            value={form.password}
+            onChange={onChangeInput}
             required
           />
         </InputContainer>
-        <button onClick={() => goToFeed(navigate)}>Entrar</button>
+        <button >Entrar</button>
         <button onClick={() => goToSignUp(navigate)}>Não tenho cadastro</button>
       </FormContainer>
     </main>
--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,24 +1,58 @@
 import { useNavigate } from "react-router-dom";
 import { goToFeed, goToLogin } from "../../routes/coordinator";
 import { FormContainer, InputContainer } from "./styled";
+import { baseURL } from "../../constants/baseURL";
+import useForm from "../../Hooks/useForm";
+import axios from "axios";
 
 function SignUpPage() {
   const navigate = useNavigate();
 
+  const { form, onChangeInput, clearFild } = useForm({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const body = {
+      name: form.name,
+      email: form.email,
+      password: form.password,
+    };
+    axios
+      .post(`${baseURL}/user/signup`, body)
+      .then((res) => {
+        goToFeed(navigate);
+        localStorage.setItem("token", res.data.token);
+        console.log(`Deu certo${res}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <main>
       <h1>Cadastro</h1>
-      <FormContainer>
+      <FormContainer onSubmit={handleRegister}>
         <InputContainer>
           <label htmlFor="name">Nome:</label>
           <input
             id="name"
+            name="name"
+            onChange={onChangeInput}
+            value={form.name}
             required
           />
         </InputContainer>
         <InputContainer>
           <label htmlFor="email">E-mail:</label>
           <input
+            name="email"
+            onChange={onChangeInput}
+            value={form.email}
             id="email"
             required
           />
@@ -27,12 +61,16 @@ function SignUpPage() {
           <label htmlFor="password">Senha:</label>
           <input
             id="password"
+            name="password"
+            onChange={onChangeInput}
+            value={form.password}
+            id="email"
             required
           />
         </InputContainer>
 
-        <button onClick={() => goToFeed(navigate)}>Cadastrar</button>
-        <button onClick={() => goToLogin(navigate)}>Já sou cadastrado</button>
+        <button>Cadastrar</button>
+        <button>Já sou cadastrado</button>
       </FormContainer>
     </main>
   );
